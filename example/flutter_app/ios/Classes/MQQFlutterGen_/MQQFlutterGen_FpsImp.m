28a98a66f0201a7389959253cf156d7a1d55d126
Dywanedu <dywanedu@tencent.com>
2021-08-05 11:03:48 +0800

    Update($pubspec) : update platform_source_gen to 0.7.5

--- example/flutter_app/ios/Classes/MQQFlutterGen_/MQQFlutterGen_FpsImp.m
+++ example/flutter_app/ios/Classes/MQQFlutterGen_/MQQFlutterGen_FpsImp.m
@@ -3,6 +3,10 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @implementation MQQFlutterGen_FpsImp
+
+/// Dart method declaraction:   Future<String> getPageName(int a)
+/// @param a Agument a, type: dart.core.int.
+/// @param callback Agument callback, type: ChannelManager.Result.
 - (void )getPageName:(int )a callback:(void(^)(NSString *))callback
  {
 	NSMutableArray *args = [NSMutableArray array];
@@ -10,12 +14,18 @@ NS_ASSUME_NONNULL_BEGIN
 	[[MQQFlutterGen_ChannelManager sharedManager] invokeMethod:@"getPageName" args:args completion:callback];
 }
 
+
+/// Dart method declaraction:   Future<double> getFps()
+/// @param callback Agument callback, type: ChannelManager.Result.
 - (void )getFps:(void(^)(NSNumber *))callback
  {
 	NSMutableArray *args = [NSMutableArray array];
 	[[MQQFlutterGen_ChannelManager sharedManager] invokeMethod:@"getFps" args:args completion:callback];
 }
 
+
+/// Dart method declaraction:   void add11(int b)
+/// @param b Agument b, type: dart.core.int.
 - (void )add11:(int )b
  {
 	NSMutableArray *args = [NSMutableArray array];
@@ -23,12 +33,19 @@ NS_ASSUME_NONNULL_BEGIN
 	[[MQQFlutterGen_ChannelManager sharedManager] invokeMethod:@"add11" args:args completion:nil];
 }
 
+
+/// Dart method declaraction:   Future<PageInfo> getPage()
+/// @param callback Agument callback, type: ChannelManager.Result.
 - (void )getPage:(void(^)(MQQFlutterGen_PageInfo *))callback
  {
 	NSMutableArray *args = [NSMutableArray array];
 	[[MQQFlutterGen_ChannelManager sharedManager] invokeMethod:@"getPage" args:args completion:callback];
 }
 
+
+/// Dart method declaraction:   Future<List<PageInfo>> getListCustom(List<InnerClass> a)
+/// @param a Agument a, type: dart.core.List.
+/// @param callback Agument callback, type: ChannelManager.Result.
 - (void )getListCustom:(NSArray<MQQFlutterGen_InnerClass *> *)a callback:(void(^)(NSArray<MQQFlutterGen_PageInfo *> *))callback
  {
 	NSMutableArray *args = [NSMutableArray array];
@@ -36,6 +53,9 @@ NS_ASSUME_NONNULL_BEGIN
 	[[MQQFlutterGen_ChannelManager sharedManager] invokeMethod:@"getListCustom" args:args completion:callback];
 }
 
+
+/// Dart method declaraction:   Future<Map<PageInfo, int>> getMapCustom()
+/// @param callback Agument callback, type: ChannelManager.Result.
 - (void )getMapCustom:(void(^)(NSDictionary<MQQFlutterGen_PageInfo *, NSNumber *> *))callback
  {
 	NSMutableArray *args = [NSMutableArray array];
--- example/flutter_app/ios/Classes/MQQFlutterGen_/native2flutter/MQQFlutterGen_Fps2.h
+++ example/flutter_app/ios/Classes/MQQFlutterGen_/native2flutter/MQQFlutterGen_Fps2.h
@@ -7,8 +7,20 @@ NS_ASSUME_NONNULL_BEGIN
 @optional
 
 
+
+/// Dart method declaraction:   Future<String> getPageName(Map<String, int> t, String t2);
+/// @param t Agument t, type: dart.core.Map.
+/// @param t2 Agument t2, type: dart.core.String.
+/// @param callback Agument callback, type: ChannelManager.Result.
 - (void )getPageName:(NSDictionary<NSString *, NSNumber *> *)t t2:(NSString *)t2 callback:(void(^)(NSString *))callback;
+
+/// Dart method declaraction:   Future<double> getFps(String t, int a);
+/// @param t Agument t, type: dart.core.String.
+/// @param a Agument a, type: dart.core.int.
+/// @param callback Agument callback, type: ChannelManager.Result.
 - (void )getFps:(NSString *)t a:(int )a callback:(void(^)(NSNumber *))callback;
+
+/// Dart method declaraction:   void add23();
 - (void )add23;
 
 @end
